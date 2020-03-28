89fbf00c8f311239dfacbd5d534cb0eadac9155d
msc111-risingstar <62786753+msc111-risingstar@users.noreply.github.com>
2020-03-28 16:31:47 +0530

    Update os_1.c
    updated on 4:30 PM 28/03/2020

--- os_1.c
+++ os_1.c
@@ -16,3 +16,40 @@ int main()
   }
   printf("Enter Time Quantum: ");
   scanf("%d",&time_quantum);
+ for(time=0,count=0;remain!=0;)
+  {
+    if(rt[count]<=time_quantum && rt[count]>0)
+    {
+      time+=rt[count];
+      rt[count]=0;
+      flag=1;
+    }
+    else if(rt[count]>0)
+    {
+        rt[count]-=time_quantum;
+      time+=time_quantum;
+    }
+    if(rt[count]==0 && flag==1)
+    {
+      remain--;
+      wait_time+=time-At[count]-bt[count];
+      turnaround_time+=time-At[count];
+      flag=0;
+    }
+    if(count==n-1)
+    {
+        x++;
+			if(x==2){
+				count=0;
+				time_quantum=6;
+			}
+			else
+				break;
+    }
+    else if(At[count+1]<=time)
+      count++;
+    else
+      count=0;
+  }
+  ms(n,rt,time,At);
+}
